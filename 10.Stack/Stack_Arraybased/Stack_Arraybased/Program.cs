1f9f0701a4ac3d8bbf6577c0c8e004ba980658e2
khaledSul <kh.suleiman99@gmail.com>
2023-05-03 14:56:03 +0300

    Add array-based stack implementation

---
+++ 10.Stack/Stack_Arraybased/Stack_Arraybased/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Stack_Arraybased
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stack = new Stack<char>();
+            Console.WriteLine("Length: " + stack.Length());
+            Console.WriteLine("Is empty?" + stack.IsEmpty());
+            stack.push('a');
+            stack.push('b');
+            stack.push('c');
+            stack.push('d');
+            stack.push('e');
+            Console.WriteLine("Is empty?"+ stack.IsEmpty());
+            Console.WriteLine("Length: " + stack.Length());
+            stack.Print();
+            stack.push('f');
+            stack.Print();
+            Console.WriteLine(stack.pop());
+            stack.Print();
+            Console.WriteLine(stack.pop());
+            stack.Print();
+            Console.WriteLine(stack.pop());
+            stack.Print();
+            Console.WriteLine(stack.pop());
+            stack.Print();
+            Console.WriteLine(stack.pop());
+            stack.Print();
+            Console.WriteLine(stack.pop());
+            Console.WriteLine("Is empty?" + stack.IsEmpty());
+            Console.WriteLine("Length: "+stack.Length());
+        }
+    }
+}
---
+++ 10.Stack/Stack_Arraybased/Stack_Arraybased/Stack.cs
@@ -0,0 +1,56 @@
+﻿namespace Stack_Arraybased
+{
+    public class Stack<T>
+    {
+        private T[] list;
+        private int _topIndex;
+        private int _initSize;
+        private int _currentSize;
+
+        public Stack()
+        {
+            _initSize = 5;
+            _currentSize = _initSize;
+            list = new T[_initSize];
+            _topIndex = -1;
+        }
+        public void push(T _data)
+        {
+            ResizeOrNot();
+            list[++_topIndex] = _data;
+        }
+        public T pop()
+        {
+            if (_topIndex == -1) return default(T)!;
+            T data = list[_topIndex--];
+            return data;
+        }
+        public T peek()
+        {
+            if (_topIndex == -1) return default(T)!;
+            return list[_topIndex];
+        }
+        public bool IsEmpty() => _topIndex == -1;
+
+        public void Print() 
+        {
+            for (int i = _topIndex; i>=0;i--)
+            {
+                Console.Write($"{list[i]}-> ");
+            }
+            Console.WriteLine();
+        }
+
+        public int Length() => _topIndex + 1;
+
+        public void ResizeOrNot()
+        {
+            if (_topIndex < _currentSize - 1) return;
+            Console.WriteLine("Resize");
+            T[] newArray = new T[_currentSize + _initSize];
+            Buffer.BlockCopy(list, 0, newArray, 0, Buffer.ByteLength(list));
+            list = newArray;
+            _currentSize += _initSize;
+        }
+    }
+}
