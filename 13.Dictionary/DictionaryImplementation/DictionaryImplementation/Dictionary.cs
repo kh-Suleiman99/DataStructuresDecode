52034b042e9688d52d7e96729d5853a67cfbed6a
khaledSul <kh.suleiman99@gmail.com>
2023-05-05 15:18:44 +0300

    Add Dictionary Implementation

---
+++ 13.Dictionary/DictionaryImplementation/DictionaryImplementation/Dictionary.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+
+namespace ByKhaled
+{
+    public class Dictionary<TKey, TValue> where TKey : class
+    {
+
+        private KeyValuePair[] entrise;
+        private int _initialSize;
+        private int _entriseCount;
+        public Dictionary()
+        {
+            _initialSize = 3;
+            entrise = new KeyValuePair[_initialSize];
+        }
+
+        public void ResizeOrNot()
+        {
+            if (_entriseCount < entrise.Length ) return;
+
+            int newSize = entrise.Length + _initialSize;
+            Console.WriteLine($"Resize from {entrise.Length} to {newSize}");
+            KeyValuePair[] newArray = new KeyValuePair[newSize];
+            Array.Copy(entrise, newArray, entrise.Length);
+            entrise = newArray;
+        }
+
+        public void Set(TKey key, TValue value)
+        {
+            for (int i =0; i< _entriseCount;i++)
+            {
+                if (entrise[i] != null && entrise[i].Key == key)
+                {
+                    entrise[i].Value = value;
+                    return;
+                }
+            }
+            ResizeOrNot();
+            KeyValuePair keyValuePair = new KeyValuePair(key, value);
+            entrise[_entriseCount] = keyValuePair;
+            _entriseCount++; 
+        }
+
+        public TValue Get(TKey key)
+        {
+            for (int i = 0; i < _entriseCount; i++)
+            {
+                if (entrise[i] != null && entrise[i].Key == key)
+                {
+                    return entrise[i].Value; 
+                }
+            }
+            return default(TValue)!;
+        }
+
+        public bool Remove(TKey key)
+        {
+            for (int i = 0; i < _entriseCount; i++)
+            {
+                if (entrise[i] != null && entrise[i].Key == key)
+                {
+                    entrise[i] = entrise[_entriseCount - 1];
+                    entrise[_entriseCount - 1] = null!;
+                    _entriseCount--;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"--------------------\nSize : {_entriseCount}");
+            for (int i = 0; i < _entriseCount; i++)
+            {
+                Console.WriteLine(entrise[i]+",");
+            }
+        }
+
+        public int Size => _entriseCount;
+        class KeyValuePair
+        {
+            TKey _key;
+            TValue _value;
+            public KeyValuePair(TKey key, TValue value)
+            {
+                _key = key;
+                _value = value;
+            }
+
+            public TKey Key
+            {
+                get { return _key; }
+            }
+
+            public TValue Value
+            {
+                get { return _value; }
+                set { _value = value; }
+            }
+
+            public override string ToString()
+            {
+                return $"{_value}";
+            }
+        }
+    }
+}
---
+++ 13.Dictionary/DictionaryImplementation/DictionaryImplementation/Program.cs
@@ -0,0 +1,38 @@
+﻿using ByKhaled;
+
+namespace DictionaryImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ByKhaled.Dictionary<string, string> dictionary = new ByKhaled.Dictionary<string, string>();
+            dictionary.Print();
+            dictionary.Set("Sinar", "sinar@gmail.com");
+            dictionary.Set("Elvis", "elvis@gmail.com");
+            dictionary.Print();
+            dictionary.Set("Tane", "tane@gmail.com");
+            dictionary.Set("Gerti", "gerti@gmail.com");
+            dictionary.Set("Arist", "arist@gmail.com");
+            dictionary.Print();
+            Console.WriteLine("-----------\n" + dictionary.Get("Tane"));
+            Console.WriteLine("-----------\n" + dictionary.Get("Arist"));
+            Console.WriteLine("-----------\n" + dictionary.Get("Arist2"));
+            dictionary.Remove("Elvis");
+            dictionary.Print();
+            dictionary.Remove("Sinar");
+            dictionary.Print();
+            dictionary.Remove("Tane");
+            dictionary.Print();
+            dictionary.Remove("Gerti");
+            dictionary.Print();
+            dictionary.Remove("Arist");
+            dictionary.Print();
+            dictionary.Set("Arist", "arist@gmail.com");
+            dictionary.Print();
+            dictionary.Set("Arist", "arist20@gmail.com");
+            dictionary.Print();
+
+        }
+    }
+}
