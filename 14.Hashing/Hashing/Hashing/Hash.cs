cc7bd8419c4db54f403d342e4ff0d87af8844ecf
khaledSul <kh.suleiman99@gmail.com>
2023-05-11 14:46:59 +0300

    Add Hashing implementation

---
+++ 14.Hashing/Hashing/Hashing/Hash.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace Hashing
+{
+    public class Hash
+    {
+        public uint Hash32(string str)
+        {
+            //This offcet Basis give us hash with 32 bit
+            uint offcetBasis = 2166136261;
+            uint FNVPrime = 16777619;
+            //transform data to Bytes
+            byte[] data = Encoding.ASCII.GetBytes(str);
+
+            uint hash = offcetBasis;
+            foreach(byte b in data)
+            {
+                hash ^= b;
+                hash *= FNVPrime;
+            }
+            Console.WriteLine(str + ", " + hash + ", " + hash.ToString("x"));
+            return hash;
+
+        }
+
+        public ulong Hash64(string str)
+        {
+            //This offcet Basis give us hash with 64 bit
+            ulong offcetBasis = 14695981039346656037;
+            ulong FNVPrime = 1099511628211;
+            //transform data to Bytes
+            byte[] data = Encoding.ASCII.GetBytes(str);
+
+            ulong hash = offcetBasis;
+            foreach (byte b in data)
+            {
+                hash ^= b;
+                hash *= FNVPrime;
+            }
+            Console.WriteLine(str + ", " + hash + ", " + hash.ToString("x"));
+            return hash;
+
+        }
+    }
+}
---
+++ 14.Hashing/Hashing/Hashing/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Hashing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hash hash= new Hash();
+            hash.Hash32("This is Original Text");
+            hash.Hash64("This is Original Text");
+        }
+    }
+}
